ebbadec73cdbe2e213f7f36a7283a9658c72d219
belozercevaa <belozerceva.a@yandex.ru>
2020-06-11 18:53:45 +0300

    the main application in which the assembly and dynamic update of all components takes place...

---
+++ src/vue_component/App.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+    div#app
+        navBar.navbar(:headerData="headerData"
+                v-on:switchTab="tab => (this.headerData.currentTab = tab)")
+
+        keep-alive
+            component.content(:is="currentTabComponent")
+
+</template>
+
+<script>
+import navBar from "./NavBar.vue";
+
+export default {
+    name: 'app',
+    data: function() {
+        return { 
+            headerData: {
+                tabs: ['GlassState', 'DiffList'],
+                currentTab: 'GlassState',
+        }}
+    },
+    computed: {
+        //при изменении currentTab меняем компонент
+        currentTabComponent() {
+            return this.headerData.currentTab;
+        }
+    },
+    components: {
+        navBar,
+        GlassState : () => import(/* webpackChunkName:"GlassState" */'./GlassState.vue'),  // функциональный импорт, webpack создаст отдельный бандл с ленивой загрузкой
+        DiffList :   () => import(/* webpackChunkName:"DiffList" */'./DiffList.vue'),      // функциональный импорт, webpack создаст отдельный бандл с ленивой загрузкой
+    }
+};
+</script>
+
+<style lang="scss">
+body {
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
