813721d7021584669af192b239a7450b2aa00ed7
belozercevaa <belozerceva.a@yandex.ru>
2020-06-13 17:50:16 +0300

    implemented module with dropdown to select a symbol for monitoring the exchange

---
+++ src/vue_component/DiffList.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+    .content
+        .symbol-type Symbol = {{ currentSymbol }}
+        .wrapper
+            select.select(v-model="currentSymbol")
+                option.select__option(v-for="(item, index) in options"
+                :key="index") {{ item }}
+
+</template>
+
+<script>
+export default {
+	name: "DiffList",
+	data: () => ({
+		options: ["BTCUSDT", "BNBBTC", "ETHBTC"],
+		currentSymbol: "BTCUSDT"
+	}),
+	watch: {
+		currentSymbol(symbol) {
+			this.$bus.$emit('symbol', symbol);
+		}
+	}
+};
+</script>
+
+<style scoped lang="scss">
+$backcolor: rgba(0, 89, 255, 0.438);
+
+.select {
+	width: 150px;
+	height: 50px;
+	background-color: $backcolor;
+	margin: 20px;
+	padding: 10px;
+	border-radius: 7px;
+}
+
+</style>
